0745b3aebbd3c3f7f2aa0e1f35fb2cf80d5d7e7b
puchochiri <doawishfor@naver.com>
2023-08-04 23:27:48 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_carrot_market_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_carrot_market_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/main_screens.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MainScreens extends StatefulWidget {
+  @override
+  _MainScreensState createState() => _MainScreensState();
+
+}
+
+class _MainScreensState extends State<MainScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: const Text('MainScreens'),
+      ),
+    );
+  }
+
+}
